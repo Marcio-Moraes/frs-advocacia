d4afd72d31fda7c26956bd6d4ee644bfd093b533
Marcio Moraes <45323822+Marcio-Moraes@users.noreply.github.com>
2022-01-03 19:05:53 -0300

    Faoro Reis Sztajnbok 1.0.0

---
+++ assets/js/menu_mobile.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
